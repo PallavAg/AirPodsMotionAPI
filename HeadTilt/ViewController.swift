d692f9564051a418dd22a0c8dd9728f80b468731
Pallav Agarwal <agpallav@gmail.com>
2022-01-28 02:05:15 +0800

    Initial Commit

--- HeadTilt/ViewController.swift
+++ HeadTilt/ViewController.swift
@@ -2,18 +2,85 @@
 //  ViewController.swift
 //  HeadTilt
 //
-//  Created by Pallav Agarwal on 1/25/22.
-//
+//  Created by Pallav Agarwal
+//  Twitter: @pallavmac
 
 import UIKit
+import CoreMotion
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CMHeadphoneMotionManagerDelegate {
 
+    @IBOutlet weak var textView: UITextView!
+    
+    let manager = CMHeadphoneMotionManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        manager.delegate = self
+        
+        print("Authorized: ", CMAuthorizationStatus.authorized)
+        
+        manager.startDeviceMotionUpdates(
+            to: OperationQueue.current!, withHandler: { [self]
+            (deviceMotion, error) -> Void in
+         
+            if let motion = deviceMotion {
+                let attitude = motion.attitude
+                let roll = degrees(attitude.roll)
+                let pitch = degrees(attitude.pitch)
+                let yaw = degrees(attitude.yaw)
+                
+                let r = motion.rotationRate
+                let ac = motion.userAcceleration
+                let g = motion.gravity
+                
+                DispatchQueue.main.async { [self] in
+                    var str = "Attitude:\n"
+                    str += degreeText("Roll", roll)
+                    str += degreeText("Pitch", pitch)
+                    str += degreeText("Yaw", yaw)
+                    
+                    str += "\nRotation Rate:\n"
+                    str += xyzText(r.x, r.y, r.z)
+                    
+                    str += "\nAcceleration:\n"
+                    str += xyzText(ac.x, ac.y, ac.z)
+                    
+                    str += "\nGravity:\n"
+                    str += xyzText(g.x, g.y, g.z)
+                    
+                    textView.text = str
+                }
+                
+            } else {
+                textView.text = "ERROR: \(error?.localizedDescription ?? "")"
+            }
+        })
     }
-
+    
+    func degreeText(_ label: String, _ num: Double) -> String {
+        return String(format: "\(label): %.0fº\n", abs(num))
+    }
+    
+    func xyzText(_ x: Double, _ y: Double, _ z: Double) -> String {
+        // Absolute value just makes it look nicer
+        var str = ""
+        str += String(format: "X: %.1f\n", abs(x))
+        str += String(format: "Y: %.1f\n", abs(y))
+        str += String(format: "Z: %.1f\n", abs(z))
+        return str
+    }
+    
+    func headphoneMotionManagerDidConnect(_ manager: CMHeadphoneMotionManager) {
+        textView.text = "AirPods Connected!"
+    }
+    
+    func headphoneMotionManagerDidDisconnect(_ manager: CMHeadphoneMotionManager) {
+        textView.text = "AirPods Disconnected :("
+    }
+    
+    func degrees(_ radians: Double) -> Double { return 180 / .pi * radians }
 
 }
 
---
+++ README.md
@@ -0,0 +1,17 @@
+# AirPods Motion API
+
+## Overview
+Swift provides an AirPods motion API that works on supported products like AirPods Pro, AirPods Gen 3, AirPods Max. 
+
+The API gives access to details like Attitude, Rotation Rate, Acceleration, and Gravity. 
+
+This Xcode project demos the use of the AirPods Motion API using `CMHeadphoneMotionManager()`.
+
+You can edit the accuracy of the values from the project. 
+
+![](https://i.imgur.com/umV38Ls.gif)
+
+## Requirements
+- Xcode
+- Swift
+- iOS 14.0+
